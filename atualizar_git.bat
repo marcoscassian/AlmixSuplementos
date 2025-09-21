13eeda4d37028b04442a312ec648852fe47b8b3a
unknown <marcozcassiano@gmail.com>
2025-09-21 20:55:57 -0300

    Atualizado automaticamente

--- atualizar_git.bat
+++ atualizar_git.bat
@@ -1,5 +1,9 @@
 @echo off
 cd C:\UsersDocuments\AlmixSuplementos
+git rm -r --cached __pycache__
+git rm -r --cached env
+git commit -m "Remove arquivos ignorados"
+git push origin main
 git add .
 git commit -m "Atualizado automaticamente"
 git push origin main

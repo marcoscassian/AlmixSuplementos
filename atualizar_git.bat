bce2ed4f5d4e6395018d9c08643b8cf6b6ec03ea
unknown <marcozcassiano@gmail.com>
2025-09-21 15:01:11 -0300

    Atualizado automaticamente

--- atualizar_git.bat
+++ atualizar_git.bat
@@ -1,5 +1,8 @@
 @echo off
 cd C:\UsersDocuments\AlmixSuplementos
+git rm -r --cached env
+git commit -m "Remove env do repositório"
+git push origin main
 git add .
 git commit -m "Atualizado automaticamente"
 git push origin main

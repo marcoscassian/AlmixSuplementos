b7b03790fecf93b861220db8856267107b30d4be
unknown <marcozcassiano@gmail.com>
2025-09-21 17:06:32 -0300

    Atualizado automaticamente

--- app.py
+++ app.py
@@ -3,10 +3,40 @@ from flask_login import LoginManager, login_user, UserMixin, login_required
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
 
+card = [
+    {
+        "titulo": "Creatina 500g Refil",
+        "preco": "R$ 120,00",
+        "precoalt": "R$ 110,00 no Pix",
+        "desconto": "10% OFF",
+        "imagem": "static/img/creatina500.webp"
+    },
+    {
+        "titulo": "Creatina 300g",
+        "preco": "R$ 80,00",
+        "precoalt": "R$ 75,00 no Pix",
+        "desconto": "5% OFF",
+        "imagem": "creatina300.jpg"
+    },
+    {
+        "titulo": "Pré-Treino",
+        "preco": "R$ 90,00",
+        "precoalt": "R$ 85,00 no Pix",
+        "desconto": "5% OFF",
+        "imagem": "pretreino.jpg"
+    },
+    {
+        "titulo": "Whey Protein Concentrado",
+        "preco": "R$ 150,00",
+        "precoalt": "R$ 140,00 no Pix",
+        "desconto": "10% OFF",
+        "imagem": "whey.jpg"
+    }
+]
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', cards=card)
 
 #marcoslindoo

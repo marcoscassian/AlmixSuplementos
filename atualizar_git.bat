73d78c18343b22f22117206321fdb0bf2731ce70
unknown <marcozcassiano@gmail.com>
2025-09-23 19:21:48 -0300

    Atualizado automaticamente

--- atualizar_git.bat
+++ atualizar_git.bat
@@ -1,8 +1,5 @@
 @echo off
-cd C:\Users\Usuário\Documents\AlmixSuplementos
-
 git add .
 git commit -m "Atualizado automaticamente"
-git push https://SHA256:5Fl8VyzVb2J9mhEIl6YckXnsqnoPTUZ4v8NwX0iEs20@github.com/marcoscassian/AlmixSuplementos.git main
-
+git push https://marcoscassian:5Fl8VyzVb2J9mhEIl6YckXnsqnoPTUZ4v8NwX0iEs20@github.com/marcoscassian/AlmixSuplementos.git main
 pause

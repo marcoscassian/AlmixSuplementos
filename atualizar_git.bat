7dabaad11997efff93b211b676566ccc524b0b32
alex <alexbrunopereira28@gmail.com>
2025-09-23 11:08:59 -0300

    Atualizado automaticamente

--- atualizar_git.bat
+++ atualizar_git.bat
@@ -1,5 +1,5 @@
 @echo off
-cd C:\Users\20231101110006\Documents\AlmixSuplementos
+cd C:\Users\20231101110039\Documents\AlmixSuplementos
 git add .
 git commit -m "Atualizado automaticamente"
 git push origin main
